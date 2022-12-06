c55a1e4e87074e5988491b4aba22941f5a837ff2
Akanksha <b.akanksha6@gmail.com>
2022-12-06 16:02:40 +0530

    thunk changes

--- src/redux/thunks.js
+++ src/redux/thunks.js
@@ -1,16 +1,20 @@
-import { setAuthToken, showAlert } from "./actions";
+import { hideLoader, setAuthToken, showAlert, showLoader } from "./actions";
 import { getAuth } from "./services";
 
 export const getAuthThunk = () => {
     return async (dispatch) => {
+        dispatch(showLoader());
         try {
             const response = await getAuth();
             if (response.status === 200) {
                 dispatch(setAuthToken(response.data.access_token));
+                dispatch(hideLoader())
             } else {
+                dispatch(hideLoader())
                 dispatch(showAlert('Authentication failed!'))
             }
         } catch (error) {
+            dispatch(hideLoader())
             dispatch(showAlert('Authentication failed!'))
         }
     };
