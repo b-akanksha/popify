f6b60a50835a025dbae57b5ff5534774f728ca2e
Akanksha <b.akanksha6@gmail.com>
2022-12-06 13:16:35 +0530

    added snackbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import InfoBar from "./components/InfoBar/InfoBar";
+import { hideAlert } from "./redux/actions";
 
 function App() {
+  const { alert: { open, message } } = useSelector(state => state.popify);
+
+  const dispatch = useDispatch();
+
+  const handleAlertClose = () => dispatch(hideAlert());
+
   return (
-    <div />
+    <>
+      <InfoBar open={open} handleClose={handleAlertClose} message={message} />
+    </>
   );
 }
 
