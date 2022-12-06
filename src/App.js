76907ab271b9ff947be8b6ec0df07fd6dc98a88f
Akanksha <b.akanksha6@gmail.com>
2022-12-06 14:09:28 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import InfoBar from "./components/InfoBar/InfoBar";
 import Loader from "./components/Loader/Loader";
-import { hideAlert } from "./redux/actions";
+import { hideAlert, setAuthToken } from "./redux/actions";
+import { getAuthThunk } from "./redux/thunks";
 
 function App() {
   const { loading, alert: { open, message } } = useSelector(state => state.popify);
@@ -10,6 +11,33 @@ function App() {
   const dispatch = useDispatch();
 
   const handleAlertClose = () => dispatch(hideAlert());
+
+  const [token, setToken] = React.useState("");
+
+  const logout = () => {
+    setToken("");
+    dispatch(setAuthToken(""));
+    window.localStorage.removeItem("token");
+  };
+
+  React.useEffect(() => {
+    const hash = window.location.hash;
+    let authToken = window.localStorage.getItem("token");
+
+    if (!authToken && hash) {
+      authToken = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", authToken);
+      dispatch(getAuthThunk());
+    }
+
+    setAuthToken(authToken);
+  }, []);
 
   return (
     <>
